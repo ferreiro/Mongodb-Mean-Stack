fe22129b6206f7d915ef7382bed827dcc90fd544
Jorge Ferreiro <me@jgferreiro.com>
2015-12-28 03:13:04 +0100

    Adding virtuals to product's

--- Chapter2_MongoDB_schema_moongose/3_schemas/product.js
+++ Chapter2_MongoDB_schema_moongose/3_schemas/product.js
@@ -24,5 +24,33 @@ var productSchema = {
 	category: Category.categorySchema
 };
 
+var schema = new mongoose.Schema(productSchema);
+
+/**** 
+	Creating one virtual:
+	A virtual is a cool feature from Mongoose that
+	allows to join different properties and returns one.
+
+	Example: Instead of asking name and surname. Mongoose can return you the 
+	full name in one query.
+*****/
+
+var currencySymbols = {
+	'EUR':'€',
+	'USD':'$',
+	'GBP':'£'
+}
+
+schema.virtual('displayPrice').get(function(){
+	return this.price.amount + currencySymbols[this.price.currency]; // add currency
+});
+
+schema.set('toObject', { virtuals: true });
+schema.set('toJSON', { virtuals: true });
+
+module.exports = schema;
+
+/*
 module.exports = new mongoose.Schema(productSchema);
 module.exports.productSchema = productSchema;
+*/
