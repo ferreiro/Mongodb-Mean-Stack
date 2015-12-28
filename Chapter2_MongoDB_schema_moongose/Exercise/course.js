c2ba2bafcf316b8e7bf939a082fc6f5230914fd1
Jorge Ferreiro <me@jgferreiro.com>
2015-12-28 03:41:46 +0100

    Updates on course.js and student.js

--- Chapter2_MongoDB_schema_moongose/Exercise/course.js
+++ Chapter2_MongoDB_schema_moongose/Exercise/course.js
@@ -11,7 +11,23 @@ var mongoose = require('mongoose');
  *                a student should take before taking this course.
  */
 var schema = new mongoose.Schema({
-  // Implement schema here!
+	// Implement schema here!
+	_id: {
+		type: String,
+		required: true
+	},
+	title: {
+		type: String,
+		required: true,
+		maxlength: 140
+	},
+	description: {
+		type: String,
+		required: true
+	},
+	requirements: [{
+		type: mongoose.Schema.Types.objectId
+	}]
 });
 
 module.exports = schema;
--- Chapter2_MongoDB_schema_moongose/Exercise/student.js
+++ Chapter2_MongoDB_schema_moongose/Exercise/student.js
@@ -9,14 +9,17 @@ var schema = new mongoose.Schema({
  * to be everything up to the first space in the student's name.
  * For instance, "William Bruce Bailey" -> "William" */
 schema.virtual('firstName').get(function() {
-  return 'Not Implemented!';
+	var split = this.name.split(' ');
+	return split[0];
 });
 
 /* Returns the student's last name, which we will define
  * to be everything after the last space in the student's name.
  * For instance, "William Bruce Bailey" -> "Bailey" */
 schema.virtual('lastName').get(function() {
-  return 'Not Implemented!';
+	var split = this.name.split(' ');
+	var surnameIndex = (split.length) - 1; // Take the last word of the name
+	return split[surnameIndex];
 });
 
 module.exports = schema;
