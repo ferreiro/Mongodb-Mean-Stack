c366d27f9004ba7bfc1139b3d167ba34fa58db70
Jorge Ferreiro <me@jgferreiro.com>
2015-12-28 01:23:17 +0100

    Category Schema created

---
+++ Chapter2_MongoDB_schema_moongose/3_schemas/category.js
@@ -0,0 +1,34 @@
+/*
+	- id (category name)
+	- parent (parent category)
+	- ancestors
+	
+	Category = {
+		_id = Electronics,
+		parent = none,
+		ancestors = [
+			'Mobiles',
+			'Tvs',
+			'Tablets'
+		]
+	}
+*/
+
+var mongoose = require('mongoose');
+
+var categorySchema = {
+	_id: {
+		type: String
+	},
+	parent: {
+		type: String,
+		ref: 'Category' // Population: http://mongoosejs.com/docs/populate.html
+	},
+	ancestors: {
+		type: String,
+		ref: 'Category' // Population: http://mongoosejs.com/docs/populate.html
+	}
+};
+
+module.exports = new mongoose.Schema(categorySchema);
+module.exports.categorySchema = categorySchema;
