142be54aa0761b683b6939574390a0a1a667541d
Jorge Ferreiro <me@jgferreiro.com>
2015-12-30 02:56:48 +0100

    Refactoring models to add new Product models

--- Chapter3_API_REST/4_Building_API/models.js
+++ Chapter3_API_REST/4_Building_API/models.js
@@ -3,14 +3,35 @@ var mongoose = require('mongoose');
 module.exports = function(wagner) {
 	mongoose.connect('mongodb://localhost:27017/test');
 
-	var categorySchema = require('./category');
-	var Category = mongoose.model('Category', categorySchema, 'categories');
+	var Category = 
+		mongoose.model('Category', require('./category'), 'categories');
+	var Product = 
+		mongoose.model('Product', require('./product'), 'products');
 
+	var models = {
+		Category: Category,
+		Product: Product
+	};
+
+	// To ensure DRY-ness, register factories in a loop
+
+	_.each(models, function(value, key) {
+		wagner.factory(key, function() {
+			return value; // register category service wiyh wagner
+		});
+	});
+
+	/*	
+	Before using the loop
 	wagner.factory('Category', function() {
 		return Category; // register category service wiyh wagner
 	});
+	*/
 
+	return models
+	/*
 	return {
 		Category: Category
 	};
+	*/
 };
